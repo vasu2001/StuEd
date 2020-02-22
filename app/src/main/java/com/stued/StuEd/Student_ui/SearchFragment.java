f0f59ac93fd069b44a8609ab9bd0ec27c234895f
Vasu Aggarwal <vasudhruv1920@gmail.com>
2020-02-22 19:10:30 +0530

    minor changes

--- app/src/main/java/com/stued/StuEd/Student_ui/SearchFragment.java
+++ app/src/main/java/com/stued/StuEd/Student_ui/SearchFragment.java
@@ -28,26 +28,23 @@ import java.util.Collections;
 public class SearchFragment extends Fragment {
     View v;
 
-    int partition(ArrayList<SlotBookingClass> arr, int low, int high)
-    {
+    int partition(ArrayList<SlotBookingClass> arr, int low, int high) {
         int pivot = arr.get(high).rating;
-        int i = (low-1); // index of smaller element
-        for (int j=low; j<high; j++)
-        {
+        int i = (low - 1); // index of smaller element
+        for (int j = low; j < high; j++) {
             // If current element is smaller than the pivot
-            if (arr.get(j).rating < pivot)
-            {
+            if (arr.get(j).rating < pivot) {
                 i++;
 
                 // swap arr[i] and arr[j]
-                Collections.swap(arr,i,j);
+                Collections.swap(arr, i, j);
             }
         }
 
         // swap arr[i+1] and arr[high] (or pivot)
-        Collections.swap(arr,i+1,high);
+        Collections.swap(arr, i + 1, high);
 
-        return i+1;
+        return i + 1;
     }
 
 
@@ -55,18 +52,16 @@ public class SearchFragment extends Fragment {
       arr[] --> Array to be sorted,
       low  --> Starting index,
       high  --> Ending index */
-    void sort(ArrayList<SlotBookingClass> arr, int low, int high)
-    {
-        if (low < high)
-        {
+    void sort(ArrayList<SlotBookingClass> arr, int low, int high) {
+        if (low < high) {
             /* pi is partitioning index, arr[pi] is
               now at right place */
             int pi = partition(arr, low, high);
 
             // Recursively sort elements before
             // partition and after partition
-            sort(arr, low, pi-1);
-            sort(arr, pi+1, high);
+            sort(arr, low, pi - 1);
+            sort(arr, pi + 1, high);
         }
     }
 
@@ -74,39 +69,36 @@ public class SearchFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        v= inflater.inflate(R.layout.activity_tutor_home,container,false );
-        DatabaseReference userReference= FirebaseDatabase.getInstance().getReference((new TinyDBorderID(getActivity())).getString("collegeName")).child("Users").child(FirebaseAuth.getInstance().getUid()).child("slots");
-        final RecyclerView recyclerView=v.findViewById(R.id.subjectList2);
-        final TextView emptyview=v.findViewById(R.id.empty_view3);
+        v = inflater.inflate(R.layout.activity_tutor_home, container, false);
+        DatabaseReference userReference = FirebaseDatabase.getInstance().getReference((new TinyDBorderID(getActivity())).getString("collegeName")).child("Users").child(FirebaseAuth.getInstance().getUid()).child("slots");
+        final RecyclerView recyclerView = v.findViewById(R.id.subjectList2);
+        final TextView emptyview = v.findViewById(R.id.empty_view3);
 
         userReference.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                ArrayList<SlotBookingClass> slots=new ArrayList<>();
-                if(dataSnapshot.exists()){
-                    for(DataSnapshot slotSnapshot: dataSnapshot.getChildren()){
+                ArrayList<SlotBookingClass> slots = new ArrayList<>();
+                if (dataSnapshot.exists()) {
+                    for (DataSnapshot slotSnapshot : dataSnapshot.getChildren()) {
                         slots.add(slotSnapshot.getValue(SlotBookingClass.class));
                         //Log.i("directory",slotSnapshot.getValue(String.class));
                     }
-                    sort(slots,0,slots.size()-1);
-                    if(slots.isEmpty())
-                    {
-                        emptyview.setText("No Slots!!");
-                        recyclerView.setVisibility(View.INVISIBLE);
-                        emptyview.setVisibility(View.VISIBLE);
-                    }
-                    else
-                    {
-                        recyclerView.setVisibility(View.VISIBLE);
-                        emptyview.setVisibility(View.INVISIBLE);
-                    }
-                    if(v.getContext()!=null) {
-                        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
-                        BookedSlotProgrammingAdapter programmingAdapter = new BookedSlotProgrammingAdapter(v.getContext(), slots);
-                        recyclerView.setAdapter(programmingAdapter);
-                    }
+                    sort(slots, 0, slots.size() - 1);
+                } else ;//showDialog();
+                if (slots.isEmpty()) {
+                    emptyview.setText("No Slots!!");
+                    recyclerView.setVisibility(View.INVISIBLE);
+                    emptyview.setVisibility(View.VISIBLE);
+                } else {
+                    recyclerView.setVisibility(View.VISIBLE);
+                    emptyview.setVisibility(View.INVISIBLE);
                 }
-                else ;//showDialog();
+                if (v.getContext() != null) {
+                    recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+                    BookedSlotProgrammingAdapter programmingAdapter = new BookedSlotProgrammingAdapter(v.getContext(), slots);
+                    recyclerView.setAdapter(programmingAdapter);
+                }
+
             }
 
             @Override
